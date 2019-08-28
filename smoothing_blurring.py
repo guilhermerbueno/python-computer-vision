b261a2e9af1462b3a52b01b4f6efc49a13559567
guilhermerbueno <gbueno91@gmail.com>
2019-08-28 16:57:27 -0300

    Blurring and smoothing images, and other low pass filters to remove noise

---
+++ smoothing_blurring.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv.imread('images/lena.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+#homogeneous filter
+kernel = np.ones((5,5), np.float32)/25
+dst = cv.filter2D(img,-1,kernel) # it's a LOW PASS FILTER that helps to remove noise
+#HIGH PASS FILTER is used to find edges in the image
+blur = cv.blur(img,(5,5))
+gaussianBlur = cv.GaussianBlur(img, (5,5), 0)
+medianBlur = cv.medianBlur(img, 5)
+bilateralFilter = cv.bilateralFilter(img, 9, 75, 75)
+
+titles = ['original','2d convolution','blurred', 'gaussian blur','median blur', 'bilateral filter']
+images = [img, dst, blur, gaussianBlur, medianBlur, bilateralFilter]
+
+for i in range(6):
+    plt.subplot(2,3,i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+cv.waitKey(0)
+cv.destroyAllWindows()
